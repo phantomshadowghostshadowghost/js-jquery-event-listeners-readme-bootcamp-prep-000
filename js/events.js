9393b7c5436fadb4e588a9fce4e66ba1577be5a0
Lamido Tijjani <kinglamido@gmail.com>
2018-04-24 13:54:59 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,28 @@
 //define functions here
+function getIt(){
+  $('p').on ('click', (click) =>
+alert(`"Hey!"`))
+  
+}
+
+function frameIt(){
+  $('img').on ('load', () =>
+   $('img').addClass('tasty')
+  )
+}
+function pressIt(){
+  $('input').on ('keydown', function (key){
+    if(key.which === 71){
+      alert('you pressed G')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on ('submit', () =>
+    alert("Your form is going to be submitted now.")
+  )
+}
 
 $(document).ready(function(){
 
