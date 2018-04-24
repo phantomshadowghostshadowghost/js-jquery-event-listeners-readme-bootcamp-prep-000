078f5a564c2db4f2e10303f1ec74e83413738297
Lamido Tijjani <kinglamido@gmail.com>
2018-04-24 13:10:49 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,15 @@
 //define functions here
+function getIt(){
+  $('p').on ('click', function(click){
+alert(`"Hey!"`)
+  })
+}
+
+function frameIt(){
+  $('img').on ('load', function(){
+  const img = $('img').attr('tasty')
+  })
+}
 
 $(document).ready(function(){
 
