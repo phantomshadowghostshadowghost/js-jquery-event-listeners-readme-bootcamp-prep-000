8af42b5faffd7128c8068f30cf6811bc98ef0905
Lamido Tijjani <kinglamido@gmail.com>
2018-04-24 13:52:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt(){
 }
 function pressIt(){
   $('input').on ('keydown', function (key){
-    if($('#typing').key.which === 71){
+    if(key.which === 71){
       alert('you pressed G')
     }
   })
